c6da635a219fd72f52778a5f4687af5ce641ade0
semi-cloud <sksmsrkdtpal@naver.com>
2022-12-02 23:35:36 +0900

    fix(PairMatchingService): 매칭 과정에서 크루 숫자가 홀수일때 인덱스 에러 해결

--- src/main/java/pairmatching/service/PairMatchingService.java
+++ src/main/java/pairmatching/service/PairMatchingService.java
@@ -19,7 +19,7 @@ public class PairMatchingService {
     public void matchPair(List<String> crews, MatchInfo matchInfo) {
         for (int i = 0; i < 3 ; i ++) {
             List<Pair> pairs = getRandomMatchCrew(crews, matchInfo);
-            if (canMatchPair(pairs, matchInfo)) {
+            if (!canMatchPair(pairs, matchInfo)) {
                 matchResult.addResult(pairs);
                 return;
             }
@@ -31,13 +31,14 @@ public class PairMatchingService {
         List<String> shuffledName = Randoms.shuffle(crews);
         List<Pair> pairs = new ArrayList<>();
 
-        for (int i = 0; i < shuffledName.size(); i += 2) {
+        for (int i = 0; i < shuffledName.size(); i += 2) { // 0 - 14까지일꺼아냐
             List<String> pair = shuffledName.subList(i, i + 2);
             pairs.add(convertToPair(matchInfo, pair));
 
             if (shuffledName.size() % 2 != 0 && i == shuffledName.size() - 3) {
                 List<String> oddPair = shuffledName.subList(i, shuffledName.size());
                 pairs.add(convertToPair(matchInfo, oddPair));
+                break;
             }
         }
         return pairs;
@@ -52,5 +53,12 @@ public class PairMatchingService {
     public boolean canMatchPair(List<Pair> randomPairs, MatchInfo matchInfo) {
         return randomPairs.stream()
                 .anyMatch(x -> matchResult.isAlreadyMatchedSameLevel(matchInfo.getLevel(), x.getCrews()));
+    }
+
+    public List<List<String>> getMatchResult(MatchInfo matchInfo) {
+        List<Pair> pairResult = matchResult.getResult(matchInfo);
+        return pairResult.stream()
+                .map(Pair::getCrewNames)
+                .collect(Collectors.toList());
     }
 }
